562847c51f1a91a5a87ed06423f7bd8899aadfc6
Krzysztof Mański <krzysztof.manski@gmail.com>
2023-01-12 15:38:59 +0100

    app

--- storage_5d/settings.py
+++ storage_5d/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storage_5d_pr'
 ]
 
 MIDDLEWARE = [
---
+++ storage_5d_pr/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Storage5DPrConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'storage_5d_pr'
